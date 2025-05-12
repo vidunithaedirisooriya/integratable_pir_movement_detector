d44434ed297193d061f05c7da6cb6709b412d3b7
vidunithaedirisooriya <vidunithaedirisooriya@gmail.com>
2025-05-12 09:58:22 +0530

    functional code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
+// Transmitter is connected to Arduino Pin #10
+// pir out on pin 8
+// passive 5v buzzer on 6
 #include <Arduino.h>
+#include <RCSwitch.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
+RCSwitch mySwitch = RCSwitch();
+unsigned int previousMillis = 0;
+// const unsigned int beepTime = 1200;
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pinMode(8, INPUT);
+  pinMode(6, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  mySwitch.enableTransmit(10);
+  mySwitch.setRepeatTransmit(3);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long currentMillis = millis();
+  if (digitalRead(8) == HIGH) {
+    mySwitch.send(MESSAGE_CORRESPONDING_TO_YOUR_SECURITY_SYSTEM, CORRESPONDING_LENGTH);
+    digitalWrite(6, HIGH);
+    delay(200);
+    digitalWrite(6, LOW);
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+//vidunithaedirisooriya 2025
